d6257848767d53faf55044e29ae145e6936f4f28
kanghanhee <kanghanhee59@gmail.com>
2022-10-16 14:39:02 +0900

    chore: 라우터 설정

--- wated-pre-onboarding-backend/routes/index.js
+++ wated-pre-onboarding-backend/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/user', require('./user'));
+router.use('/company', require('./company'));
+router.use('/post', require('./post'));
 
 module.exports = router;
